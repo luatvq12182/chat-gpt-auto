6764ecf8a3bc8104fe95d9d2a1d0dd8dca121281
Eren Yeager <eren_yeager@Erens-MacBook-Pro.local>
2023-04-10 02:52:58 +0700

    update shell

--- be/release.sh
+++ be/release.sh
@@ -1,4 +1,5 @@
-docker build -t chat-gpt-tool .
 docker stop chat-gpt-tool-ins
 docker rm chat-gpt-tool-ins
+docker image rm chat-gpt-tool
+docker build -t chat-gpt-tool .
 docker run --name chat-gpt-tool-ins -dp 3001:3001 chat-gpt-tool
